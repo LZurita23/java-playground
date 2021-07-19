0455d2cdb8b81f4edc611aba6ad5872cd55ea4f9
Lori Zurita <lorizurita23@gmail.com>
2021-07-19 17:42:59 -0500

    add code to email formatter

--- src/main/java/com/statefarm/Application.java
+++ src/main/java/com/statefarm/Application.java
@@ -2,6 +2,20 @@ package com.statefarm;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        String name = args[0];
+        String email = args[1];
+
+
+
+        if(args[0].equals("") && args[1].equals("")) {
+            System.out.println("Please provide a name and an email.");
+        }else if (args[0].equals("")) {
+            System.out.println("Please provide a name.");
+        }else if (args[1].equals("")){
+            System.out.println("Please provide an email.");
+        }else {
+            System.out.println(args[0] + " <" + args[1] + ">");
+        }
     }
+
 }
