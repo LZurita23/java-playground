61d58c399af3b375b4a495d4001b10e6800c333a
Lori Zurita <lorizurita23@gmail.com>
2021-07-20 12:14:01 -0500

    updated code and new gradlew build.

--- src/main/java/com/statefarm/Application.java
+++ src/main/java/com/statefarm/Application.java
@@ -1,18 +1,13 @@
 package com.statefarm;
 
 public class Application {
+
     public static void main(String[] args) {
-        String name = args[0];
-        String email = args[1];
 
-
-
-        if(args[0].equals("") && args[1].equals("")) {
+        if(args.length < 1) {
             System.out.println("Please provide a name and an email.");
-        }else if (args[0].equals("")) {
-            System.out.println("Please provide a name.");
-        }else if (args[1].equals("")){
-            System.out.println("Please provide an email.");
+        }else if(args.length == 1 && args.length != 2){
+            System.out.println("Please provide an email for " + args[0]);
         }else {
             System.out.println(args[0] + " <" + args[1] + ">");
         }
